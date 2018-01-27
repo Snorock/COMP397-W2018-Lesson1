2ca60e33cdd6afe9508b458e13aeb9c32b01fb99
snorock <ibrovche@gmail.com>
2018-01-27 14:39:16 -0500

    first commit

---
+++ Scripts/core/index.js
@@ -0,0 +1,22 @@
+//IIFE - Imidiately Invoked Function Expressions
+//let - veriable exists only inside function
+(function(){
+    function Init(){
+
+    }
+
+    function Start(){
+
+    }
+
+    function Update(){
+
+    }
+
+    function Main(){
+
+    }
+
+    window.onload = Init;
+
+})();
