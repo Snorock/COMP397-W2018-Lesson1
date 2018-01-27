d617735f5f4fd2f3956dd40dcbb44a5fb546e597
snorock <ibrovche@gmail.com>
2018-01-27 15:25:30 -0500

    end of class

--- Scripts/core/index.js
+++ Scripts/core/index.js
@@ -1,14 +1,23 @@
 //IIFE - Imidiately Invoked Function Expressions
 //let - veriable exists only inside function
 (function () {
+    var canvas = document.getElementById("canvas");
+    var stage;
     function Init() {
-        console.log("Game started");
+        Start();
     }
     function Start() {
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60fps
+        createjs.Ticker.on("tick", Update);
+        Main();
     }
     function Update() {
+        stage.update();
     }
     function Main() {
+        console.log("Game started.....");
+        // hellolabel = new createjs.Text("Hello World", "40px")
     }
     window.onload = Init;
 })();
--- Scripts/core/index.ts
+++ Scripts/core/index.ts
@@ -1,20 +1,29 @@
 //IIFE - Imidiately Invoked Function Expressions
 //let - veriable exists only inside function
 (function(){
+
+let canvas = document.getElementById("canvas");
+let stage: createjs.Stage;
+
     function Init():void{
-        console.log("Game started");
+        
+        Start();
     }
 
     function Start():void{
-
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60fps
+        createjs.Ticker.on("tick", Update);
+        Main();
     }
 
     function Update():void{
-
+        stage.update();
     }
 
     function Main():void{
-
+        console.log("Game started.....");
+        // hellolabel = new createjs.Text("Hello World", "40px")
     }
 
     window.onload = Init;
